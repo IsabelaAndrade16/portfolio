55d02dc91f389051d12f92b8840475d33a022d42
Isabela Andrade <isaandradesantos01@gmail.com>
2026-02-24 10:52:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,12 @@ function revealOnScroll() {
     });
 }
 
+
 window.addEventListener('scroll', revealOnScroll);
+
+  const toggle = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
